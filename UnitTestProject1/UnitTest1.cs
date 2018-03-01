fe26212561e8f26cae0282e10f680ad229b9846b
edkim2 <ekim10203@gmail.com>
2018-03-01 16:33:40 -0600

    Initial commit.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            customList list = new customList();
+            // arrange
+
+            // act
+
+            // assert
+
+        }
+    }
+}
