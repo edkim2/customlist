1a5e6fc2a2aeece75737ecc0e676b98724765766
edkim2 <ekim10203@gmail.com>
2018-03-05 09:39:20 -0600

    Got my add method approved. Created a method to make the array bigger.

--- CustomList/customList.cs
+++ CustomList/customList.cs
@@ -8,17 +8,42 @@ namespace CustomList
 {
     public class customList<T>
     {
+        // member variables
+        int count;
+        int capacity = 5;
+        T[] temporaryArray; // maybe move this to a method called CreatingNewArray
+        T[] finalArray;
 
+
+        // constructor
         public customList()
         {
-
+            capacity = 5;
+            temporaryArray = new T[0];
+            finalArray = new T[capacity];
         }
         
+        // member methods
         public void Add(T itemToAdd)
         {
 
         }
 
+        public void CreateBiggerArray()
+        {
+            T[] temproraryArray = new T[capacity * 2];
+            for(int i = 0; i < count; i++)
+            {
+                temporaryArray[i] = finalArray[i];
+            }
+            capacity = capacity * 2;
+            finalArray = temporaryArray;
+        }
+
+        public void Remove(T itemToRemove)
+        {
+
+        }
 
     }
 }
